b346043bbc18f0afbbb34478ca2b385bed2c6cc5
Agnes Brettl <brettl1991@gmail.com>
2026-03-27 21:55:03 +0000

    first commit

---
+++ project-one/src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <header>
+    <h1>Quote generator</h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  color: #ccd6d9;
+  margin-bottom: 25px;
+}
+</style>
